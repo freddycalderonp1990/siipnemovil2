1afe22526793f54b4e181b4a74bb87be4026cecf
Freddy Mac <freddycalderon1990@gmail.com>
2026-09-22 12:09:59 -0500

    verificar gps

--- lib/feactures/gps/presentation/bloc/gps/gps_bloc.dart
+++ lib/feactures/gps/presentation/bloc/gps/gps_bloc.dart
@@ -8,38 +8,38 @@ import 'package:permission_handler/permission_handler.dart';
 part 'gps_event.dart';
 part 'gps_state.dart';
 
+
 class GpsBloc extends Bloc<GpsEvent, GpsState> {
+
   StreamSubscription? gpsServiceSubscription;
 
-  GpsBloc()
-    : super(
-        const GpsState(isGpsEnabled: false, isGpsPermissionGranted: false),
-      ) {
-    on<GpsAndPermissionEvent>(
-      (event, emit) => emit(
-        state.copyWith(
-          isGpsEnabled: event.isGpsEnabled,
-          isGpsPermissionGranted: event.isGpsPermissionGranted,
-        ),
-      ),
+
+  GpsBloc() : super( const GpsState(isGpsEnabled: false, isGpsPermissionGranted: false ) ) {
+
+    on<GpsAndPermissionEvent>((event, emit) => emit( state.copyWith(
+        isGpsEnabled: event.isGpsEnabled,
+        isGpsPermissionGranted: event.isGpsPermissionGranted
+    ))
     );
 
     _init();
+
   }
 
   Future<void> _init() async {
+
     final gpsInitStatus = await Future.wait([
       _checkGpsStatus(),
       _isPermissionGranted(),
     ]);
 
-    add(
-      GpsAndPermissionEvent(
-        isGpsEnabled: gpsInitStatus[0],
-        isGpsPermissionGranted: gpsInitStatus[1],
-      ),
-    );
+
+    add( GpsAndPermissionEvent(
+      isGpsEnabled: gpsInitStatus[0],
+      isGpsPermissionGranted: gpsInitStatus[1],
+    ));
   }
+
 
   Future<bool> _isPermissionGranted() async {
     final isGranted = await Permission.location.isGranted;
@@ -47,53 +47,29 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
   }
 
   Future<bool> _checkGpsStatus() async {
+
     final isEnable = await Geolocator.isLocationServiceEnabled();
 
     //nos suscribimos para saber si el gps del dispositivo esta activo o no
-    gpsServiceSubscription = Geolocator.getServiceStatusStream().listen((
-      event,
-    ) {
-      final isEnabled = (event.index == 1) ? true : false;
-      add(
-        GpsAndPermissionEvent(
-          isGpsEnabled: isEnabled,
-          isGpsPermissionGranted: state.isGpsPermissionGranted,
-        ),
-      );
+    gpsServiceSubscription = Geolocator.getServiceStatusStream().listen((event) {
+      final isEnabled = ( event.index == 1 ) ? true : false;
+      add( GpsAndPermissionEvent(
+        isGpsEnabled: isEnabled,
+        isGpsPermissionGranted: state.isGpsPermissionGranted,
+      ));
     });
 
     return isEnable;
   }
 
-  Future<void> askGpsAccess() async {
-    final before = await Permission.location.status;
 
-    print('====================================');
-    print('PERMISO ANTES: $before');
-    print('isGranted: ${before.isGranted}');
-    print('isDenied: ${before.isDenied}');
-    print('isPermanentlyDenied: ${before.isPermanentlyDenied}');
-    print('isRestricted: ${before.isRestricted}');
-    print('====================================');
+  Future<void> askGpsAccess() async {
 
     final status = await Permission.location.request();
 
-    print('====================================');
-    print('PERMISO DESPUÉS: $status');
-    print('isGranted: ${status.isGranted}');
-    print('isDenied: ${status.isDenied}');
-    print('isPermanentlyDenied: ${status.isPermanentlyDenied}');
-    print('isRestricted: ${status.isRestricted}');
-    print('====================================');
-
-    switch (status) {
+    switch ( status ) {
       case PermissionStatus.granted:
-        add(
-          GpsAndPermissionEvent(
-            isGpsEnabled: state.isGpsEnabled,
-            isGpsPermissionGranted: true,
-          ),
-        );
+        add( GpsAndPermissionEvent(isGpsEnabled: state.isGpsEnabled, isGpsPermissionGranted: true) );
         break;
 
       case PermissionStatus.denied:
@@ -101,19 +77,19 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
       case PermissionStatus.limited:
       case PermissionStatus.provisional:
       case PermissionStatus.permanentlyDenied:
-        add(
-          GpsAndPermissionEvent(
-            isGpsEnabled: state.isGpsEnabled,
-            isGpsPermissionGranted: false,
-          ),
-        );
+        add( GpsAndPermissionEvent(isGpsEnabled: state.isGpsEnabled, isGpsPermissionGranted: false) );
         openAppSettings();
+
     }
+
   }
+
+
 
   @override
   Future<void> close() {
     gpsServiceSubscription?.cancel();
     return super.close();
   }
+
 }
--- lib/feactures/gps/presentation/bloc/gps/gps_state.dart
+++ lib/feactures/gps/presentation/bloc/gps/gps_state.dart
@@ -1,6 +1,7 @@
 part of 'gps_bloc.dart';
 
 class GpsState extends Equatable {
+
   final bool isGpsEnabled;
   final bool isGpsPermissionGranted;
 
@@ -8,20 +9,21 @@ class GpsState extends Equatable {
 
   const GpsState({
     required this.isGpsEnabled,
-    required this.isGpsPermissionGranted,
+    required this.isGpsPermissionGranted
   });
 
-  GpsState copyWith({bool? isGpsEnabled, bool? isGpsPermissionGranted}) =>
-      GpsState(
-        isGpsEnabled: isGpsEnabled ?? this.isGpsEnabled,
-        isGpsPermissionGranted:
-            isGpsPermissionGranted ?? this.isGpsPermissionGranted,
-      );
+  GpsState copyWith({
+    bool? isGpsEnabled,
+    bool? isGpsPermissionGranted,
+  }) => GpsState(
+      isGpsEnabled: isGpsEnabled ?? this.isGpsEnabled,
+      isGpsPermissionGranted: isGpsPermissionGranted ?? this.isGpsPermissionGranted
+  );
 
   @override
-  List<Object> get props => [isGpsEnabled, isGpsPermissionGranted];
+  List<Object> get props => [ isGpsEnabled, isGpsPermissionGranted ];
 
   @override
-  String toString() =>
-      '{ isGpsEnabled: $isGpsEnabled, isGpsPermissionGranted: $isGpsPermissionGranted }';
+  String toString() => '{ isGpsEnabled: $isGpsEnabled, isGpsPermissionGranted: $isGpsPermissionGranted }';
+
 }
